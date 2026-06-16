6e30fcdcfb4c39c17f806051b51eeac047365faf
Anu-yadav923 <anuy8306@gmail.com>
2026-06-16 14:14:05 +0530

    MiddleWares Learning

--- Express_learning/app.js
+++ Express_learning/app.js
@@ -2,30 +2,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res) =>{
-    res.send("Home Page");
+const auth = (req,res,next) => {
+    console.log("checking User");
+    next();
+};
+
+app.get("/",(req,res) =>{
+    res.send("home page");
 });
 
 app.get("/about",(req,res) =>{
     res.send("About Page");
 });
 
-app.get("/contact",(req,res) =>{
-    res.send("Contact Page");
+app.get("/admin",auth,(req,res) =>{
+    res.send("Admin Page");
 });
 
-app.get("/users/:id",(req,res) =>{
-    
-    console.log(req.params.id);
-    res.send("Done");
-});
-
-app.get("/search",(req,res) =>{
-    console.log(req.query.name)
-    res.send("Done");
-});
-
-
-app.listen(3000, () =>{
-    console.log("Server Running");
-});
+app.listen(3000);
